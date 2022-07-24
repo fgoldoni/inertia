90e62051e4a6520f3b6f90ec0078ab731e763a04
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-24 19:49:42 +0200

    initial commit

--- Modules/Comments/Database/Migrations/2022_07_24_150421_create_comments_table.php
+++ Modules/Comments/Database/Migrations/2022_07_24_150421_create_comments_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Traits\Database\DisableForeignKeys;
+use App\Traits\Database\TruncateTable;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
