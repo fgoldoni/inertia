a611f7a540929f05037f5bf258f32512da7449bc
Goldoni <fotsa.goldoni@yahoo.fr>
2022-07-27 18:45:30 +0200

    initial commit

--- Modules/Teams/Http/Controllers/TeamsController.php
+++ Modules/Teams/Http/Controllers/TeamsController.php
@@ -214,8 +214,7 @@ class TeamsController extends Controller
 
     public function restore(int $id)
     {
-        $team = null;
-        $job = $this->teamsRepository->withCriteria([
+        $team = $this->teamsRepository->withCriteria([
             new WithTrashed(),
         ])->restore($id);
 
